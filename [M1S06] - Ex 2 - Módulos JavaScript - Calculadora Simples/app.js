0b70b955b29869e2b52101fff8e808a31caeb4c8
Fernando Constantino <118141170+Fernando-Shandow@users.noreply.github.com>
2025-07-20 20:18:43 -0300

    Add files via upload

---
+++ [M1S06] - Ex 2 - Módulos JavaScript - Calculadora Simples/app.js
@@ -0,0 +1,9 @@
+import { somar, subtrair, multiplicar, dividir } from "./matematica.js";
+
+const a = 10;
+const b = 5;
+
+console.log(`Soma: ${somar(a, b)}`);
+console.log(`Subtração: ${subtrair(a, b)}`);
+console.log(`Multiplicação: ${multiplicar(a, b)}`);
+console.log(`Divisão: ${dividir(a, b)}`);
